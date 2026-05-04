bc9ca5b3fb7b739e6faeb9bb513980a29158ea85
Micha Johannes Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-05-04 13:31:22 +0200

    residue pair distribution plot docs

--- content/docs/plotting/_meta.js
+++ content/docs/plotting/_meta.js
@@ -2,6 +2,7 @@ export default {
   'index': 'Introduction',
   'plot_crosslink_type_distribution': 'Plotting Crosslink Type Distribution',
   'plot_peptide_pair_distribution': 'Plotting Peptide Pair Distribution',
+  'plot_residue_pair_distribution': 'Plotting Residue Pair Distribution',
   'plot_protein_distribution': 'Plotting Protein Distribution',
   'plot_score_distribution': 'Plotting Score Distribution',
   'plot_target_decoy_distribution': 'Plotting Target and Decoy Distribution',
